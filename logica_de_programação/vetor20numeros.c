59421c2d1b87b40d71ed7cac0d35af308ab24725
Malleficarum | Laís <100413256+malleficarum@users.noreply.github.com>
2022-09-29 16:29:56 -0300

    Create vetor20numeros.c

---
+++ logica_de_programação/vetor20numeros.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+ 
+int main() {
+
+    int i,n,v[20]; // data types.
+
+    for(i=0;i<=20;i++){
+       scanf("%d",&v[20]); // entrada dos números do vetor.
+    }
+
+    for(i=0;i<=20;i++){
+        printf("%d.\n",v[20]); // saída dos números do vetor.
+    }
+  
+    system("pause"); 
+    return 0;
+}
